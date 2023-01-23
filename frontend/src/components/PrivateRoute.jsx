fd5a58bb15fddbd3b2a52763658a5afbc7675c80
gitirom <wadiaromdane10@gmail.com>
2023-01-23 14:18:51 +0100

    i add the spinner icon in loading part

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 
 import { Outlet , Navigate } from 'react-router-dom';
 import  UseAuthStatus from '../hooks/useAuthStatus';
+import Spinner from './Spinner';
 
 
 const  PrivateRoute =() =>{
     const { loggedIn, checkingStatus } = UseAuthStatus();
 
     if(checkingStatus){
-        return <h3>Loading...</h3>
+        return <Spinner /> ;                               // its just a loading  icon                      
     }
     
     return  loggedIn ? <Outlet /> : <Navigate to="/Sing-In" /> ;                  /*if the user is sign-in is gone render the profile page  // React-Outlet provides two components which aid in cross-component transclusion */
